03506067ec74b9742b45e44ca4f4d41e8b3d4b2b
Ronald Pandolfi <ronpandolfi@gmail.com>
2022-03-22 13:15:24 -0700

    Fix source paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -102,8 +102,8 @@ echo ::endgroup::
 # sphinx extensions
 if [ "$INPUT_INSTALL_EXTENSIONS" = true ] ; then
     echo ::group::Installing sphinx extensions
-    echo "pip3 install -r $docs_src/$INPUT_SOURCE_DIR/requirements.txt"
-    pip3 install -r $docs_src/$INPUT_SOURCE_DIR/requirements.txt
+    echo "pip3 install -r $docs_src/requirements.txt"
+    pip3 install -r $docs_src/requirements.txt
     echo ::endgroup::
 fi
 
